dc952194e807ead4cd5e9efce5a0b80e3eddc0cb
Kevin Wells <wellska1@rose-hulman.edu>
2011-09-17 20:27:13 -0400

    Add javadoc example

--- src/MobileDirectory/src/edu/rosehulman/android/directory/C.java
+++ src/MobileDirectory/src/edu/rosehulman/android/directory/C.java
@@ -1,5 +1,12 @@
 package edu.rosehulman.android.directory;
 
+/** Constants used throughout the application */
 public class C {
+	/** Tag used for Logging.
+	 *
+	 * <pre>
+	 * Log.d(C.TAG, &quot;Debug Message&quot;);
+	 * </pre>
+	 */
 	public static String TAG = "MobileDirectory";
 }
