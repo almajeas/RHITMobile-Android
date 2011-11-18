32321fa961fbc74b48e71e87f3f293bc71114e93
Kevin Wells <wellska1@rose-hulman.edu>
2011-11-17 23:53:11 -0500

    Add ref counting to db handles

--- src/MobileDirectory/src/edu/rosehulman/android/directory/db/TableAdapter.java
+++ src/MobileDirectory/src/edu/rosehulman/android/directory/db/TableAdapter.java
@@ -1,27 +1,34 @@
 package edu.rosehulman.android.directory.db;
 
-import edu.rosehulman.android.directory.MyApplication;
+import java.util.HashMap;
+import java.util.Map;
+
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import edu.rosehulman.android.directory.MyApplication;
 
-/**
- * Provide generic functionality to all database adapters
- */
+/** Provide generic functionality to all database adapters */
 public abstract class TableAdapter {
+
+	private static Map<SQLiteDatabase, Integer> dbCount;
 
 	private SQLiteOpenHelper dbOpenHelper;
 	
+	static {
+		dbCount = new HashMap<SQLiteDatabase, Integer>();
+	}
+
 	/** Shared db connection */
 	protected SQLiteDatabase db;
-	
+
 	/**
 	 * Creates a new TableAdapter
 	 */
 	public TableAdapter() {
 		dbOpenHelper = DatabaseHelper.getInstance();
 	}
-	
+
 	/**
 	 * Creates a new TableAdapter with an existing database connection
 	 * 
@@ -29,29 +36,32 @@ public abstract class TableAdapter {
 	 */
 	public TableAdapter(SQLiteDatabase db) {
 		this.db = db;
+		ref(db);
 	}
-	
+
 	/**
 	 * Open a writable version of the database
 	 */
 	public void open() {
 		if (db != null) {
-			db.close();
+			deref(db);
 			db = null;
 		}
 		db = dbOpenHelper.getWritableDatabase();
-	
+		ref(db);
+
 		verifyThread();
 	}
-	
+
 	/**
 	 * Close an open database connection.
 	 * 
-	 * TableAdapters created with an existing connection should
-	 * not close the connection to the database.
+	 * TableAdapters created with an existing connection should not close the
+	 * connection to the database.
 	 */
 	public void close() {
-		db.close();
+		deref(db);
+		db = null;
 	}
 
 	/**
@@ -61,12 +71,12 @@ public abstract class TableAdapter {
 	 * @return the String contained in the only cell
 	 */
 	protected String getString(Cursor cursor) {
-		assert(cursor.getColumnCount() == 1);
-		assert(cursor.getCount() == 1);
+		assert (cursor.getColumnCount() == 1);
+		assert (cursor.getCount() == 1);
 		cursor.moveToFirst();
 		return cursor.getString(0);
 	}
-	
+
 	/**
 	 * Retrieve a boolean value from the database
 	 * 
@@ -76,14 +86,15 @@ public abstract class TableAdapter {
 	 */
 	protected boolean getBoolean(Cursor cursor, int column) {
 		int val = cursor.getInt(column);
-		assert(val == 0 || val == 1);
+		assert (val == 0 || val == 1);
 		return val == 1;
 	}
-	
+
 	/**
 	 * Convert an array of strings to a list of columns
 	 * 
-	 * @param args A list of column names
+	 * @param args
+	 *            A list of column names
 	 * @return A string separating the column names with commas
 	 */
 	protected String columns(String... args) {
@@ -95,28 +106,32 @@ public abstract class TableAdapter {
 		builder.setCharAt(builder.length() - 1, ' ');
 		return builder.toString();
 	}
-	
+
 	/**
 	 * Format a table/column pair
 	 * 
-	 * @param table The table to use
-	 * @param column The column to use
+	 * @param table
+	 *            The table to use
+	 * @param column
+	 *            The column to use
 	 * @return table.column
 	 */
 	protected String column(String table, String column) {
 		return table + "." + column;
 	}
-	
+
 	//protected String tables(String... args) {
 	//	//TODO implement
 	//	return args[0];
 	//}
-	
+
 	/**
 	 * Format a table name/alias pair
 	 * 
-	 * @param name The table name
-	 * @param alias The alias of the table
+	 * @param name
+	 *            The table name
+	 * @param alias
+	 *            The alias of the table
 	 * 
 	 * @return name alias
 	 */
@@ -127,16 +142,37 @@ public abstract class TableAdapter {
 	private static void verifyThread() {
 		if (!MyApplication.CHECK_UI_THREAD)
 			return;
-		
+
 		//make sure we are not the main thread
 		StackTraceElement stack[] = Thread.currentThread().getStackTrace();
 		for (StackTraceElement frame : stack) {
-			if ("main".equals(frame.getMethodName()) && 
-					frame.getClassName().contains("NativeStart") && 
-					frame.isNativeMethod()) {
+			if ("main".equals(frame.getMethodName()) &&
+				frame.getClassName().contains("NativeStart") &&
+				frame.isNativeMethod()) {
 				throw new RuntimeException("DB access on UI thread");
 			}
 		}
 	}
 	
+	private synchronized static void ref(SQLiteDatabase db) {
+		int count = 0;
+		
+		if (dbCount.containsKey(db)) {
+			count = dbCount.get(db);
+		}
+		
+		dbCount.put(db, count + 1);
+	}
+	
+	private synchronized static void deref(SQLiteDatabase db) {
+		int count = dbCount.get(db);
+		
+		if (count == 1) {
+			dbCount.remove(db);
+			db.close();
+		} else {
+			dbCount.put(db, count - 1);
+		}
+	}
+
 }
