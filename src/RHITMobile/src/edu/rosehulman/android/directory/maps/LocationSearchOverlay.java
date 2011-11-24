123c43d0e240d065e639547d6a4d578c5e9a29da
Kevin Wells <wellska1@rose-hulman.edu>
2011-11-23 23:28:28 -0500

    Show location screen on search result press
    refs #224

--- src/RHITMobile/src/edu/rosehulman/android/directory/maps/LocationSearchOverlay.java
+++ src/RHITMobile/src/edu/rosehulman/android/directory/maps/LocationSearchOverlay.java
@@ -3,18 +3,24 @@ package edu.rosehulman.android.directory.maps;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.content.Context;
+import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapView;
 import com.google.android.maps.OverlayItem;
 import com.readystatesoftware.mapviewballoons.BalloonItemizedOverlay;
 
 import edu.rosehulman.android.directory.C;
+import edu.rosehulman.android.directory.LocationActivity;
 import edu.rosehulman.android.directory.model.Location;
 
 public class LocationSearchOverlay extends BalloonItemizedOverlay<OverlayItem> implements ManageableOverlay{
 
+	private static final int MIN_ZOOM_LEVEL = 19;
+	
 	private OverlayManagerControl manager;
 	
 	private List<SearchItem> items;
@@ -49,7 +55,34 @@ public class LocationSearchOverlay extends BalloonItemizedOverlay<OverlayItem> i
 	public int size() {
 		return items.size();
 	}
+	
+	@Override
+	protected void animateTo(int index, GeoPoint center) {
+		if (manager != null) {
+			manager.markSelected();
+		}
+		MapView mapView = getMapView();
+		ViewController controller = new ViewController(mapView);
+		Point pt = new Point(mapView.getWidth() / 2, mapView.getHeight() / 4 * 3);
+		controller.animateTo(center, pt, MIN_ZOOM_LEVEL + 1, true);
+	}
 
+	@Override
+	protected boolean onBalloonTap(int index, OverlayItem item) {
+		final Location loc = items.get(index).location;
+		
+		new PopulateLocation(new Runnable() {
+			
+			@Override
+			public void run() {
+				Context context = getMapView().getContext();
+				context.startActivity(LocationActivity.createIntent(context, loc));
+			}
+		}).execute(loc);
+		
+		return true;
+	}
+	
 	@Override
 	public void clearSelection() {
 		Log.d(C.TAG, "Clearing selection");
