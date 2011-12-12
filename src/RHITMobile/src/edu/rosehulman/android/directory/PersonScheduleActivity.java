518f41e1a5cd64952259836e5cee3cc426a39bbe
Kevin Wells <wellska1@rose-hulman.edu>
2011-12-11 19:54:51 -0500

    Update prototype UI for schedules

--- src/RHITMobile/src/edu/rosehulman/android/directory/PersonScheduleActivity.java
+++ src/RHITMobile/src/edu/rosehulman/android/directory/PersonScheduleActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TabHost;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class PersonScheduleActivity extends TabActivity {
@@ -62,16 +64,45 @@ public class PersonScheduleActivity extends TabActivity {
 
 	TabHost.TabContentFactory tabFactory = new TabHost.TabContentFactory() {
 		
+		private TableRow makeRow(String tag, int i) {
+			
+			String times[] = new String[] {"", "8:05", "9:00", "9:55", "10:50", "11:45", "12:40", "1:35", "2:30", "3:25", "4:20"};
+
+			LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			View v = inflater.inflate(R.layout.schedule_table_row, null);
+
+			TextView hour = (TextView)v.findViewById(R.id.hour);
+			TextView time = (TextView)v.findViewById(R.id.time);
+			TextView course = (TextView)v.findViewById(R.id.course);
+			TextView room = (TextView)v.findViewById(R.id.room);
+
+			hour.setText(String.valueOf(i));
+			time.setText(times[i]);
+			if ("wed".equals(tag)) {
+				if (i >= 7 && i <= 9) {
+					course.setText("CSSE498-01");
+					room.setText("O205");
+				}
+			} else {
+				if (i == 9) {
+					course.setText("CSSE474-02");
+					room.setText("G317");
+				}
+			}
+			return (TableRow)v;
+		}
+		
 		@Override
 		public View createTabContent(String tag) {
 			LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			View v = inflater.inflate(R.layout.schedule_table, null);
+			View root = inflater.inflate(R.layout.schedule_table, null);
+			TableLayout table = (TableLayout)root.findViewById(R.id.table);
 			
-			TextView name = (TextView)v.findViewById(R.id.tag);
+			for (int i = 1; i < 11; i++) {
+				table.addView(makeRow(tag, i));
+			}
 			
-			name.setText(tag);
-			
-			return v;
+			return root;
 		}
 	};
 }
