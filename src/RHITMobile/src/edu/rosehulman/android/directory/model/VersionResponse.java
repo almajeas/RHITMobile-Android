715f39303f06034bf2e50563822e5d28f0ebd9ac
Kevin Wells <wellska1@rose-hulman.edu>
2012-02-02 12:32:21 -0500

    Update server version model

--- src/RHITMobile/src/edu/rosehulman/android/directory/model/VersionResponse.java
+++ src/RHITMobile/src/edu/rosehulman/android/directory/model/VersionResponse.java
@@ -17,6 +17,11 @@ public class VersionResponse {
 	 * The version of Campus service data
 	 */
 	public String services;
+
+	/**
+	 * The version of tour tags
+	 */
+	public String tags;
 	
 	/**
 	 * Create a version response from a JSON structure
@@ -28,8 +33,9 @@ public class VersionResponse {
 	public static VersionResponse deserialize(JSONObject root) throws JSONException {
 		VersionResponse res = new VersionResponse();
 		
-		res.locations = root.getString("ServerVersion");
+		res.locations = root.getString("LocationsVersion");
 		res.services = root.getString("ServicesVersion");
+		res.tags = root.getString("TagsVersion");
 		
 		return res;
 	}
